ff4a5b47dd275f347e2ebf4a847afaebe05e285f
th3-5had0w <theshadow127001@gmail.com>
2021-04-28 14:26:18 +0700

    update

--- main.py
+++ main.py
@@ -16,23 +16,24 @@ document = requests.get('https://redpillvn.com/kho-luu-tru-bai-viet-redpillvn/')
 death = open('blacklist.txt', 'a')
 
 
-for link in bs(document.text, 'html.parser').find_all('a'):
-    if (link.get('href') != None):
-        if ('forum.redpillvn.com' not in link.get('href')):
-            blacklist = open('blacklist.txt', 'r').readlines()
-            checklink = link.get('href')+'\n'
-            if checklink not in blacklist:
-                #pdf = weasyprint.HTML(link.get('href')).write_pdf()
-                #open(link.title, 'wb').write(pdf)
-                title = link.text
-                if '#' in title:
-                    title = re.sub('[#]', '', title)
-                if '/' in title:
-                    title = re.sub('[/]', '', title)
-                path = 'pdfs/'+title
-                pdf = weasyprint.HTML(link.get('href')).write_pdf()
-                open(path, 'wb').write(pdf)
-                death.write(checklink)
-    print('Done! -', link.get('href'))
-
-print(blacklist)
+while True:
+    for link in bs(document.text, 'html.parser').find_all('a'):
+        if (link.get('href') != None):
+            if ('forum.redpillvn.com' not in link.get('href')):
+                blacklist = open('blacklist.txt', 'r').readlines()
+                checklink = link.get('href')+'\n'
+                if checklink not in blacklist:
+                    #pdf = weasyprint.HTML(link.get('href')).write_pdf()
+                    #open(link.title, 'wb').write(pdf)
+                    title = link.text
+                    if '#' in title:
+                        title = re.sub('[#]', '', title)
+                    if '/' in title:
+                        title = re.sub('[/]', '', title)
+                    path = 'pdfs/'+title
+                    pdf = weasyprint.HTML(link.get('href')).write_pdf()
+                    open(path, 'wb').write(pdf)
+                    death.write(checklink)
+        print('Done! -', link.get('href'))
+    print(blacklist)
+    sleep(259200)
